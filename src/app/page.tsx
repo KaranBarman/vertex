4e026e2ffa18014996fbf2bc87f69007b6a3f2e6
karanbarman <hello.karandev@gmail.com>
2024-02-10 13:34:00 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <div className="max-w-min mx-auto mt-32">
         <h1 className="hero_font text-4xl md:text-[200px] font-bold">VERTEX</h1>
-        <p className="hero_font text-muted-foreground text-center text-sm md:text-lg">
+        <p className="hero_font text-muted-foreground text-center text-sm md:text-lg mt-20">
           Elevate Your Digital Presence with Expert Web Development Services
         </p>
       </div>
