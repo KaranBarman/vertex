6b08b5b2b3bbe85baf8e371e8808f29ab6c126b9
karanbarman <hello.karandev@gmail.com>
2024-02-10 18:49:40 +0530

    nav

--- src/components/page/hero.tsx
+++ src/components/page/hero.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 const Hero = () => {
-  return (
-    <div>
-      <div className="max-w-fit md:max-w-min mx-auto mt-32">
-        <h1 className="hero_font text-4xl md:text-[200px] font-bold">VERTEX</h1>
-        <p className="hero_font text-muted-foreground text-center text-sm md:text-lg mt-20">
-          Elevate Your Digital Presence with Expert Web Development Services
-        </p>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Hero;
