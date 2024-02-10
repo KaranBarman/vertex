3d72db833f51447f21b07e03f69cc1f234a9bd60
karanbarman <hello.karandev@gmail.com>
2024-02-10 21:41:24 +0530

    nav

--- src/components/page/hero.tsx
+++ src/components/page/hero.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex rounded-md py-10 px-2 flex-col bg-gradient-to-r my-10 from-cyan-500 to-blue-500">
       <div className="mx-auto">
         <Image
           src={"/images/logo/vertex.png"}
           alt="vertex"
           height={500}
           width={500}
-          className="dark:brightness-0 dark:invert"
+          className="brightness-0 invert"
         />
-        <p className="text-center text-muted-foreground">
+        <p className="text-center text-muted-foreground text-black">
           Elevate Your Digital Presence with Expert Web Development Services
         </p>
       </div>
