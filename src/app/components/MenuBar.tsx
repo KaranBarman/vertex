824d009c3eaaf0950f1806ec74976163c0f7760e
karanbarman <hello.karandev@gmail.com>
2024-03-02 13:28:59 +0530

    ok

--- src/app/components/MenuBar.tsx
+++ src/app/components/MenuBar.tsx
@@ -43,7 +43,7 @@ export function NavigationMenuDemo() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>About us</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+            <ul className="grid w-[300px] gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
                   <a
