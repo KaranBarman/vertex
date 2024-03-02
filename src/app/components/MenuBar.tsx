5f7ccc0c7bae14c1f586ac709a47b67db288c51b
karanbarman <hello.karandev@gmail.com>
2024-03-02 13:27:17 +0530

    ok

--- src/app/components/MenuBar.tsx
+++ src/app/components/MenuBar.tsx
@@ -81,7 +81,7 @@ export function NavigationMenuDemo() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>What we do</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul className="grid w-[300px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
                 <ListItem
                   key={component.title}
